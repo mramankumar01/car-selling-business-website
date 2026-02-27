6a97cfc1153750e3abc91cee462066d7c7209df3
Aman Kumar <mramankumar215@gmail.com>
2026-02-27 20:09:07 +0530

    completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty@2154',
+        'HOST': 'localhost',
     }
 }
 
@@ -120,3 +123,7 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'carzone/static',
 ]
+
+# media settings
+MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_URL = '/media/'
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    linkedin = models.URLField(max_length=150)
+    twitter = models.URLField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.first_name +" "+ self.last_name
